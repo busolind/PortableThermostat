1a00196e35512cc57acc42c871d5dd4c5b43d7c5
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-02-16 14:07:33 +0100

    Unita prova display e termometro

--- src/mobile_unit/mobile_unit.ino
+++ src/mobile_unit/mobile_unit.ino
@@ -1,9 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <SSD1306Wire.h>
+
 const char *ssid = "IOT_TEST";
 const char *password = "IOT_TEST";
-const char *mqtt_server = "192.168.1.100";
+const char *mqtt_server = "192.168.178.100";
 
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
@@ -11,6 +17,27 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SDA D2
+#define SCL D1
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);  // ADDRESS, SDA, SCL, OLEDDISPLAY_GEOMETRY  -  Extra param required for 128x32 displays.
+
+#define DHTPIN D5
+#define DHTTYPE    DHT11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+#define PLUSBTTN D8
+int lastPlusRead = 0;
+int currentPlusRead;
+int targetTemp=18;
+
+uint32_t delayMS;
+
+
+
 
 void setup_wifi() {
 
@@ -82,12 +109,28 @@ void mqtt_reconnect() {
 void setup() {
     Serial.begin(115200);
     pinMode(BUILTIN_LED, OUTPUT);
+    pinMode(SDA, OUTPUT);
+    pinMode(SCL, OUTPUT);
+    pinMode(DHTPIN, OUTPUT);
+    pinMode(PLUSBTTN, OUTPUT);
+  
     setup_wifi();
     mqtt_client.setServer(mqtt_server, 1883);
     mqtt_client.setCallback(mqtt_callback);
+
+    dht.begin();
+    sensor_t sensor;
+  
+  // Set delay
+  delayMS = 50;
+
+  display.init();
+  //display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
 }
 
 void loop() {
+    delay(delayMS);
     if (!mqtt_client.connected()) {
         mqtt_reconnect();
     }
@@ -100,6 +143,45 @@ void loop() {
         snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
         Serial.print("Publish message: ");
         Serial.println(msg);
-        mqtt_client.publish("PortableThermostat/static/hello", msg);
+        mqtt_client.publish("PortableThermostat/mobile/hello", msg);
     }
+
+    display.clear();
+
+    // Get temperature event and print its value.
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+        Serial.println(F("Error reading temperature!"));
+        display.drawString(0, 0, "T:E");
+    }
+    else {
+        Serial.print(F("Temperature: "));
+        Serial.print(event.temperature);
+        Serial.println(F("°C"));
+        display.drawString(0, 0, "T: " + String(event.temperature) + "°C");
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+        Serial.println(F("Error reading humidity!"));
+        display.drawString(0, 15, "H:E");
+    }
+    else {
+        Serial.print(F("Humidity: "));
+        Serial.print(event.relative_humidity);
+        Serial.println(F("%"));
+        display.drawString(0, 15, "H: " + String(event.relative_humidity) + "%");
+    }
+  
+  
+    //Senza debounce (c'è giusto il delay, per i test funziona)
+    currentPlusRead=digitalRead(PLUSBTTN);
+    if(currentPlusRead == 1 && (currentPlusRead != lastPlusRead)){
+        targetTemp++;
+    }
+    lastPlusRead=currentPlusRead;
+    display.drawString(64, 0, "TRGET:" + String(targetTemp) + "°C");
+    
+    display.display();
 }
