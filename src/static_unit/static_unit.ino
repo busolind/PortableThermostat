6b0bed13978f6b873f150497808ca29a1d2e9a58
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-03-05 17:37:13 +0100

    Modificato MQTT su static con PubSubClientTools

--- src/static_unit/static_unit.ino
+++ src/static_unit/static_unit.ino
@@ -1,30 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <PubSubClientTools.h>
 #include <TaskScheduler.h>
 
 const char *ssid = "IOT_TEST";
 const char *password = "IOT_TEST";
 const char *mqtt_server = "192.168.178.10";
 
-unsigned long last_reconnect_attempt = 0;
 #define RECONNECT_DELAY 5000
-const char *cmdtopic = "PortableThermostat/cmd/mobile";
-const char *infotopic = "PortableThermostat/info/static/currentlyOn";
+String cmdtopic_mobile_base = "PortableThermostat/cmd/mobile";
+String infotopic = "PortableThermostat/info/static/currentlyOn";
 
 WiFiClient espClient;
-PubSubClient mqtt_client(espClient);
-#define MSG_BUFFER_SIZE (50)
-char msg[MSG_BUFFER_SIZE];
-
-#define TOPIC_BUFFER_SIZE (150)
-char topic_buffer[TOPIC_BUFFER_SIZE];
+PubSubClient mqtt_client(mqtt_server, 1883, espClient);
+PubSubClientTools mqtt_tools(mqtt_client);
 
 bool turnOn = false;
 short currentlyOn = -1;
 
 #define RELAY_PIN D5
 #define NUMBER_OF_MOBILES (5)
-short mobiles[NUMBER_OF_MOBILES]; //Memorizza lo stato dei termostati: -1 se unknown, 0 se non richiedono riscaldamento, 1 se lo richiedono
+int mobiles[NUMBER_OF_MOBILES]; //Memorizza lo stato dei termostati: -1 se unknown, 0 se non richiedono riscaldamento, 1 se lo richiedono
 unsigned long last_updates[NUMBER_OF_MOBILES]; //Usato per "invecchiamento" degli stati dei termostati. Se un termostato non comunica per più di un certo numero di ms si resetta il suo stato a unknown
 #define AGING_THRESHOLD 300000
 
@@ -55,42 +51,29 @@ void setup_wifi() {
     digitalWrite(BUILTIN_LED, HIGH);
 }
 
-void mqtt_callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-    }
-    Serial.println();
-    
+void mqtt_callback(String topic, String message) {
+    Serial.println("Message arrived [" + topic + "]: " + message);
     digitalWrite(BUILTIN_LED, LOW);
-    int index = atoi((char*)&topic[(strlen(cmdtopic) + 1)]); //Estrae il numero del termostato mobile dal percorso del topic
+    int index = topic.substring(cmdtopic_mobile_base.length()+1, topic.indexOf('/', cmdtopic_mobile_base.length()+1)).toInt();
 
     if(index < NUMBER_OF_MOBILES){
-        mobiles[index] = (payload[0] - '0');
+        mobiles[index] = message.toInt();
         last_updates[index] = millis();
     }
-    delay(50);
     digitalWrite(BUILTIN_LED, HIGH);
 }
 
 void mqtt_subscribe_to_mobiles(){
-    snprintf(topic_buffer, TOPIC_BUFFER_SIZE, "%s/+/turnOn", cmdtopic);
-    Serial.print("Subscribed to topic [");
-    Serial.print(topic_buffer);
-    Serial.println("]");
-    mqtt_client.subscribe(topic_buffer);
+    String subTo = cmdtopic_mobile_base + "/+/turnOn";
+    Serial.println("Subscribed to topic [" + subTo + "]");
+    mqtt_tools.subscribe(subTo, mqtt_callback);
 }
 
 void mqtt_reconnect() {
     digitalWrite(BUILTIN_LED, LOW);
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (mqtt_client.connect(clientId.c_str())) {
+    if (mqtt_client.connect("ESP8266Client-PT-static")) {
         Serial.println("connected");
         digitalWrite(BUILTIN_LED, HIGH);
         // ... and resubscribe
@@ -103,7 +86,7 @@ void mqtt_reconnect() {
 Task mqtt_reconnect_task(RECONNECT_DELAY * TASK_MILLISECOND, TASK_FOREVER, mqtt_reconnect);
 
 //Inizializza l'array dei mobiles a -1
-void init_mobile_arr(short mobilearr[], int mobilecount){
+void init_mobile_arr(int mobilearr[], int mobilecount){
     for(int i=0; i<mobilecount; i++){
         mobilearr[i]=-1;
     }
@@ -143,10 +126,9 @@ void handle_relay(){
 Task handle_relay_task(TASK_IMMEDIATE, TASK_FOREVER, handle_relay);
 
 void publish_currentlyOn(){
-    itoa(currentlyOn, msg, 10);
-    Serial.print("Publish message [" + String(infotopic) + "]: ");
-    Serial.println(msg);
-    mqtt_client.publish(infotopic, msg);
+    String pub = String(currentlyOn);
+    Serial.println("Publish message [" + infotopic + "]: " + pub);
+    mqtt_tools.publish(infotopic, pub);
 }
 Task publish_currentlyOn_task(10 * TASK_SECOND, TASK_FOREVER, publish_currentlyOn);
 
@@ -159,8 +141,6 @@ void setup() {
     digitalWrite(RELAY_PIN, LOW);
     currentlyOn = 0;
     setup_wifi();
-    mqtt_client.setServer(mqtt_server, 1883);
-    mqtt_client.setCallback(mqtt_callback);
     init_mobile_arr(mobiles, NUMBER_OF_MOBILES);
 
     ts.addTask(mqtt_reconnect_task);
